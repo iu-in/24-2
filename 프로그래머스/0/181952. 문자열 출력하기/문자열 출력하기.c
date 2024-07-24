a76b8a58ed2f1c0cf4dd55b8fe5b1d9ef45f77c3
이우인 <165548810+iu-in@users.noreply.github.com>
2024-07-24 13:47:51 +0900

    [level 0] Title: 문자열 출력하기, Time: 2.88 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define LEN_INPUT 1000001
+
+int main(void) {
+    char s1[LEN_INPUT];
+    // 문자열 입력 받기
+    scanf("%s", s1);
+    
+    // 문자열 출력하기
+    printf("%s\n", s1);
+
+    return 0;
+}
